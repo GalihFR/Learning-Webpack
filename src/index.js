747b3db2ee0d50ffb7fc9df1e4c47052585dafa1
GalihFR <galihfathul29@gmail.com>
2020-12-14 12:00:35 +0700

    Fundamental Webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './styles/style.css'
+import './styles/style.scss'
+
+// const template = React.createElement('h1',null,'Dashboard COVID-19');
+
+const App = () => {
+    return <h1>Learning Webpack</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
